751d65b12eaf124804333b34fa682f4b12040cb0
Nanak Bajwa <nanak.bajwa@gmail.com>
2018-03-03 02:14:19 -0700

    Persisting data using Codable protocol and Plist.

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -12,30 +12,13 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
-    
-    
-    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Itesm.plist")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-      let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Find Mike2"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Find Mike3"
-        itemArray.append(newItem3)
-    
-        
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-        itemArray = items
-       }
+        loadItems()
         
         
     }
@@ -51,17 +34,16 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        let item = itemArray[indexPath.row]
+    let item = itemArray[indexPath.row]
         
-        cell.textLabel?.text = item.title
+    cell.textLabel?.text = item.title
         
-        cell.accessoryType = item.done ? .checkmark : .none              // Ternary operator for doing if else statements in one line. value = condition ? valueIfTrue : valueIfFalse. dont even     need to put "== true" in front of item.done
+    cell.accessoryType = item.done ? .checkmark : .none              // Ternary operator for doing if else statements in one line. value = condition ? valueIfTrue : valueIfFalse. dont even     need to put "== true" in front of item.done
         
         return cell
         
-    
     }
 
     //MARK - Tableview delegate methods
@@ -74,8 +56,7 @@ class TodoListViewController: UITableViewController {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.reloadData()
-        
+        self.saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
@@ -98,20 +79,42 @@ class TodoListViewController: UITableViewController {
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            
-            self.tableView.reloadData()
+        self.saveItems()
         }
         
         alert.addAction(action)
-        
-        
         present(alert, animated: true, completion: nil)
         
     }
     
     
+    
+    func saveItems () {
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("errir encoding")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    func loadItems () {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("error")
+            }
+        }
+    }
     
     
     
--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -9,12 +9,10 @@
 import Foundation
 
 
-class Item {
+class Item: Codable {
     
     var title : String = ""
     
     var done : Bool = false
-
-
     
 }
